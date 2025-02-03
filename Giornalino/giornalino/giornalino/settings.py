8bd88aaedbb4b37f1907bd8173340cdd78485a26
Luca <75177548+Lucacarrino@users.noreply.github.com>
2025-02-03 20:21:44 +0100

    Update settings.py

--- Giornalino/giornalino/giornalino/settings.py
+++ Giornalino/giornalino/giornalino/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-pfx-fqssn#iir0l2#6nq7v1uyrmren9(nhkbbc4l+dws)ld2yc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
